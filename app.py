32365bcbf0cd7c32d3dee8e9fbca621bc79f24bf
akawyg <wonyugeon2012@gmail.com>
2026-01-15 14:35:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
 from pathlib import Path
 
-st.set_page_config(page_title="베이킹 셀러", layout="wide")
+st.set_page_config(
+    page_title="베이킹 셀러",
+    layout="wide"
+)
 
 BASE_DIR = Path(__file__).resolve().parent
+
+st.title("🍞 베이킹 상품 목록")
+st.write("원하시는 상품을 선택하세요.")
 
 products = [
     {
@@ -11,18 +17,16 @@ products = [
         "name": "상품 1",
         "price": "10,000원",
         "image": BASE_DIR / "images" / "product1_1.jpg",
-        "page": "pages/product_1.py"
+        "page": "pages/1_product_1.py"
     },
     {
         "id": 2,
         "name": "상품 2",
         "price": "20,000원",
         "image": BASE_DIR / "images" / "product2_1.jpg",
-        "page": "pages/product_2.py"
+        "page": "pages/2_product_2.py"
     }
 ]
-
-st.title("🍞 베이킹 상품 목록")
 
 cols = st.columns(len(products))
 
