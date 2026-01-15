d4c94a37b417ea78280921b4202072050c173cfb
akawyg <wonyugeon2012@gmail.com>
2026-01-15 14:08:36 +0900

    Update 상품_목록1.py

--- pages/상품_목록1.py
+++ pages/상품_목록1.py
@@ -1,3 +1,17 @@
+col1, col2, col3 = st.columns([1, 1, 1])
+
+with col1:
+    if st.button("🏠 메인"):
+        st.switch_page("app.py")
+
+with col2:
+    if st.button("🛒 상품"):
+        st.switch_page("pages/1_상품_목록.py")
+
+with col3:
+    if st.button("📦 주문"):
+        st.switch_page("pages/2_상품_상세.py")
+
 import streamlit as st
 
 st.set_page_config(layout="wide")
